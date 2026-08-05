f4f2d62ba16932d4573ee005e333e5c0caadc28e
starsgiri <giriofkala2004@gmail.com>
2026-08-05 07:27:51 +0530

    added the solution

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -3,6 +3,8 @@ package com.linkedin.collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class Application {
 
@@ -17,13 +19,20 @@ public class Application {
 		oxford.setPetFriendly(true);
 		victoria.setPetFriendly(true);
 		
-		for(Room room : rooms) {
-			
-			if(room.isPetFriendly()) {
-				System.out.println(room.getName());
-			}
-			
-		}
+		rooms.stream()
+				.filter(new Predicate<Room>() {
+					@Override
+					public boolean test(Room room) {
+						System.out.format("%s is pet friendly %b",room.getName(),room.isPetFriendly());
+						return room.isPetFriendly();
+					}
+				}).forEach(new Consumer<Room>() {
+					@Override
+					public void accept(Room room) {
+						System.out.println(room.getName());
+
+					}
+				});
 		
 	}
 }
