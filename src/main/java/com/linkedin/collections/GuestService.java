67ffceb123ecdbda7a01a9d7c67262bf3a227908
starsgiri <giriofkala2004@gmail.com>
2026-08-06 22:15:27 +0530

    played with the list mainly the methods under the array list of set

--- src/main/java/com/linkedin/collections/GuestService.java
+++ src/main/java/com/linkedin/collections/GuestService.java
@@ -14,8 +14,11 @@ public class GuestService {
 		 *  who have indicated the provided room as the first preference in their preferred
 		 *  room list. 
 		 */
-		
-		return null; 
+		List<Guest> match =  new ArrayList<>();
+		for(Guest g: guests){
+			if(g.getPreferredRooms().equals(room)) match.add(g);
+		}
+		return match;
 
 	}
 
@@ -26,7 +29,18 @@ public class GuestService {
 		 *  ahead of those guests not in the program. Otherwise, guests are arranged in the
 		 *  order they were inserted.
 		 */
-
+		if(guest.isLoyaltyProgramMember() && !this.checkinList.isEmpty()){
+			int i=0;
+			for(;i < this.checkinList.size();i++){
+				if(this.checkinList.get(i).isLoyaltyProgramMember()){
+					continue;
+				}
+				break;
+			}
+			this.checkinList.add(i,guest);
+		}else{
+			this.checkinList.add(guest);
+		}
 	}
 	
 	public void swapPosition(Guest guest1, Guest guest2) {
@@ -35,7 +49,12 @@ public class GuestService {
 		 *  3.  Swaps the position of the two provided guests within the checkinList.
 		 *  If guests are not currently in the list no action is required.
 		 */ 
-
+		if(this.checkinList.contains(guest1) && this.checkinList.contains(guest2)){
+			int pos1 = this.checkinList.indexOf(guest1);
+			int pos2 = this.checkinList.indexOf(guest2);
+			this.checkinList.set(pos1,guest2);
+			this.checkinList.set(pos2,guest1);
+		}
 	}
 
 	public List<Guest> getCheckInList() {
