8597eefcaeab52d94e7365da2b84b7f3c030fdc4
starsgiri <giriofkala2004@gmail.com>
2026-08-06 21:28:23 +0530

    played well with treeset => implemented differneted types of methods undertreeset part

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -2,12 +2,24 @@ package com.linkedin.collections;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.NavigableSet;
+import java.util.TreeSet;
 
 public class Application {
 
 	public static void main(String[] args) {
 		
 		List<Integer> numbers = Arrays.asList(500, 1500, 2500, 1000, 3000, 2000);
-		
+		NavigableSet<Integer> numberTree = new TreeSet<>(numbers);
+		numberTree.headSet(1750)
+		.stream().forEach(System.out::println);
+		System.out.println();
+		numberTree.tailSet(2500).stream()
+				.forEach(System.out::println);
+		System.out.println();
+		numberTree.subSet(1500,2600).stream().forEach(System.out::println);
+		System.out.println();
+		System.out.println(numberTree.lower(1600 ));
+		System.out.println(numberTree.higher(1234));
 	}
 }
