9ad230dfbfa3574e605f91a729d377df5ab5080f
starsgiri <giriofkala2004@gmail.com>
2026-08-08 08:05:13 +0530

    played with the Dequeue methods

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,13 +1,24 @@
 package com.linkedin.collections;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class Application {
 
 	public static void main(String[] args) {
-		
-		
-		
+
+		Deque<String> messageDeck = new ArrayDeque<>();
+		messageDeck.push("message 1");
+
+		messageDeck.push("message 2");
+
+		messageDeck.push("message 3");
+		messageDeck.push("message 4");
+
+		messageDeck.push("message 5");
+
+		print(messageDeck);
+		System.out.println(messageDeck.pop());
 	}
 
 	public static void print(Deque<String> deque) {
