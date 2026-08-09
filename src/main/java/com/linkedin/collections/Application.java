6704064dcf1f6a874136f90208f5f6a46940bc58
starsgiri <giriofkala2004@gmail.com>
2026-08-09 06:44:13 +0530

    Solution added

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,5 +1,7 @@
 package com.linkedin.collections;
 
+import java.util.HashMap;
+
 public class Application {
 
 	public static void main(String[] args) {
@@ -9,7 +11,10 @@ public class Application {
 
 		Guest john = new Guest("John", "Doe", false);
 		Guest maria = new Guest("Maria", "Doe", true);
-
+		Map<Room,Guest> assignment = new HashMap<>();
+		assignment.put(piccadilly,john);
+		assignment.put(oxford,maria);
+		
 		
 				
 	}
